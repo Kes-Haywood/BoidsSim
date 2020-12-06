b50deb30ea3325996d50f7bff08dbb13d44958de
Kes Haywood <kes.haywood@gmail.com>
2020-12-06 13:44:37 +1100

    project start

---
+++ BoidsSim/BoidsSim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoidsSim
+//
+//  Created by Kes Haywood on 6/12/20.
+//  Copyright © 2020 Kes Haywood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
